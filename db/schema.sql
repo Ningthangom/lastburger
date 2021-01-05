1c79404b3848f00d2cfc221bc210e2994ac199b1
Ningthangom <ningthangom@gmail.com>
2021-01-05 18:27:59 +1030

    added db

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(150) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (id)
+);
+
